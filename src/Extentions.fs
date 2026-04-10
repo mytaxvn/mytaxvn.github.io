bce2b74674ef7d8a75c05d306873bde6b94d3ef0
Nghia <nghiabuivan1987@gmail.com>
2026-04-10 13:29:23 +0700

    refactor

--- src/Extentions.fs
+++ src/Extentions.fs
@@ -6,35 +6,31 @@ open Fable.Core
 open Fastoch.Feliz
 
 type Html with
-    static member label (text: string) =
-        Html.label [ prop.text text ]
+    static member label(text: string) = Html.label [ prop.text text ]
 
 module List =
-    let changeAt i map list =
-        list |> List.updateAt i (map list[i])
+    let changeAt i map list = list |> List.updateAt i (map list[i])
 
 module Results =
     let allOk xs = xs |> Seq.forall Result.isOk
 
-type Result<'a,'b> with
+type Result<'a, 'b> with
     member this.Value =
         match this with
         | Ok value -> value
         | Error _ -> failwith "OOPS"
 
-let roundInt64 (x: float) =
-    x |> Math.Round |> int64
+let roundInt64 (x: float) = x |> Math.Round |> int64
 
 type private NumberFormat =
     abstract format: int64 -> string
 
 [<Emit("new Intl.NumberFormat('vi-VN')")>]
-let private createFormatter (): NumberFormat = jsNative
+let private createFormatter () : NumberFormat = jsNative
 
-let private formatter = createFormatter()
+let private formatter = createFormatter ()
 
-let formatNumber (value: int64) =
-    formatter.format(value)
+let formatNumber value = formatter.format value
 
 let tryParseInt64 (input: string) : int64 option =
     match Int64.TryParse input with
